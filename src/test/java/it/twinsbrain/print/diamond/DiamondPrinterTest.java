ee33c0ad8cf05262aa49cdde7f766f7294592e24
Paolo Sciarra <itbhps@gmail.com>
2015-07-15 22:20:14 +0200

    refactored test

--- src/test/java/it/twinsbrain/print/diamond/DiamondPrinterTest.java
+++ src/test/java/it/twinsbrain/print/diamond/DiamondPrinterTest.java
@@ -31,41 +31,31 @@ public class DiamondPrinterTest {
 
     @Test
     public void diamond_for_a() throws Exception {
-        DiamondPrinter printer = DiamondPrinter.forChar('A');
-        String diamond = printer.print();
-
+        String diamond = DiamondPrinter.forChar('A').print();
         assertThat(diamond, is(equalTo("A")));
     }
 
     @Test
     public void diamond_for_b() throws Exception {
-        DiamondPrinter printer = DiamondPrinter.forChar('B');
-        String diamond = printer.print();
-
+        String diamond = DiamondPrinter.forChar('B').print();
         assertThat(diamond, is(equalTo(" A \nB B\n A ")));
     }
 
     @Test
     public void diamond_for_c() throws Exception {
-        DiamondPrinter printer = DiamondPrinter.forChar('C');
-        String diamond = printer.print();
-
+        String diamond = DiamondPrinter.forChar('C').print();
         assertThat(diamond, is(equalTo("  A  \n B B \nC   C\n B B \n  A  ")));
     }
 
     @Test
     public void diamond_for_d() throws Exception {
-        DiamondPrinter printer = DiamondPrinter.forChar('D');
-        String diamond = printer.print();
-
+        String diamond = DiamondPrinter.forChar('D').print();
         assertThat(diamond, is(equalTo("   A   \n  B B  \n C   C \nD     D\n C   C \n  B B  \n   A   ")));
     }
 
     @Test
     public void diamond_for_e() throws Exception {
-        DiamondPrinter printer = DiamondPrinter.forChar('E');
-        String diamond = printer.print();
-
+        String diamond = DiamondPrinter.forChar('E').print();
         assertThat(diamond, is(equalTo("    A    \n   B B   \n  C   C  \n D     D \nE       E\n D     D \n  C   C  \n   B B   \n    A    ")));
     }
 
