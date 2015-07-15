86d8ccb9d290b3a463cc0a9e0f8583ecbe1fe78f
Paolo Sciarra <itbhps@gmail.com>
2015-07-15 19:53:39 +0200

    added inital test

---
+++ src/test/java/it/twinsbrain/print/diamond/PrintDiamondTest.java
@@ -0,0 +1,28 @@
+package it.twinsbrain.print.diamond;
+
+/**
+ * Given a letter print a diamond starting with 'A'
+ * with the supplied letter at the widest point.
+ * <p>
+ * For example: print-diamond 'E' prints
+ * <p>
+ *     A
+ *    B B
+ *   C   C
+ *  D     D
+ * E       E
+ *  D     D
+ *   C   C
+ *    B B
+ *     A
+ * <p>
+ * For example: print-diamond 'C' prints
+ * <p>
+ *   A
+ *  B B
+ * C   C
+ *  B B
+ *   A
+ */
+public class PrintDiamondTest {
+}
